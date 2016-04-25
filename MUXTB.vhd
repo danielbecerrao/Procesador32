21fe9ed9b7499152758bb1d3bfe6dd07a2ee8431
ghostcomando <daniel-3223@hotmail.com>
2016-04-25 18:46:27 -0500

    MUX y su TB

---
+++ MUXTB.vhd
@@ -0,0 +1,97 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:46:12 04/25/2016
+-- Design Name:   
+-- Module Name:   D:/Biblioteca/Documents/Procesador/Procesador32/MUXTB.vhd
+-- Project Name:  Procesador
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: MUX
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY MUXTB IS
+END MUXTB;
+ 
+ARCHITECTURE behavior OF MUXTB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT MUX
+    PORT(
+         RfToMux : IN  std_logic_vector(31 downto 0);
+         inm : IN  std_logic;
+         seuToMux : IN  std_logic_vector(31 downto 0);
+         MuxToAlu : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal RfToMux : std_logic_vector(31 downto 0) := (others => '0');
+   signal inm : std_logic := '0';
+   signal seuToMux : std_logic_vector(31 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal MuxToAlu : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   constant <clock>_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: MUX PORT MAP (
+          RfToMux => RfToMux,
+          inm => inm,
+          seuToMux => seuToMux,
+          MuxToAlu => MuxToAlu
+        );
+
+   -- Clock process definitions
+   <clock>_process :process
+   begin
+		<clock> <= '0';
+		wait for <clock>_period/2;
+		<clock> <= '1';
+		wait for <clock>_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      wait for <clock>_period*10;
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
---
+++ SEUTB.vhd
@@ -0,0 +1,81 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:42:04 04/25/2016
+-- Design Name:   
+-- Module Name:   D:/Biblioteca/Documents/Procesador/Procesador32/SEUTB.vhd
+-- Project Name:  Procesador
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: SEU
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY SEUTB IS
+END SEUTB;
+ 
+ARCHITECTURE behavior OF SEUTB IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT SEU
+    PORT(
+         simm13 : IN  std_logic_vector(12 downto 0);
+         seu32 : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal simm13 : std_logic_vector(12 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal seu32 : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: SEU PORT MAP (
+          simm13 => simm13,
+          seu32 => seu32
+        );
+
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+
+      -- insert stimulus here 
+
+      wait;
+   end process;
+
+END;
